b1de855d61d5229f86afdcd7a441b52bc79cd563
Jake Irvine <me@jakeirvine.com>
2016-12-12 18:20:02 +0000

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,3 +1,3 @@
-url = "https://api.github.com/repos/veggiedefender/commitment-issues/contents/punchingbag.md"
+url = "https://api.github.com/repos/Mrnumbers9/commitment-issues/contents/punchingbag.md"
 
-token = "qwertyuiopasdfghjklzxcvbnm0123456789abcd"        
+token = "d979cbc3071060b2f416561c36a562a0417bb5b5"        
